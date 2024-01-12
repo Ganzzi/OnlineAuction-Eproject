d1a7a1cc8edb2fac1837e38b9eaf88d8064d9583
Truong <truongnqts2201002@fpt.edu.vn>
2024-01-12 21:16:53 +0700

    add fake user data

--- ClientApp/components/Dashboard/users/UserList.tsx
+++ ClientApp/components/Dashboard/users/UserList.tsx
@@ -1,6 +1,21 @@
+import { User } from '@/types/models/user'
 import React from 'react'
 
+
+
 type Props = {}
+const user: User[]=[
+  {
+    userId: 1,
+    username: 'sjhbcj',
+    email:"sdbfvjshbd" ,
+    password: "jsbdckjsbd",
+    role: "jdbsjshbd",
+    refreshToken:"jdbsjshbd",
+    locked: true,
+  },
+]
+  
 
 const UserList = (props: Props) => {
   return (
