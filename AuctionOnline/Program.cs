4bc53ce0e8b50f564c213746a97d107f3ad33f11
hzost <hzost09@gmail.com>
2024-01-18 18:55:48 +0700

    update them jwt

--- AuctionOnline/Program.cs
+++ AuctionOnline/Program.cs
@@ -1,5 +1,8 @@
-using Application;
+﻿using Application;
 using Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +15,35 @@ builder.Services.AddControllers().AddJsonOptions(x =>
 //Add configServcie 
 builder.Services.AddAppService(builder.Configuration);  
 builder.Services.AddInfrastructureServices(builder.Configuration);
+
+//jwt token
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    // không cần https để trao đổi metadata
+    x.RequireHttpsMetadata = false;
+    //save token vào AuthenticationProperties
+    x.SaveToken = true;
+    //định dạng chính xác việc xét jwt 
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        //Điều khiển việc xác thực SecurityKey đã ký securityToken. Nếu giá trị này là true, SecurityKey sẽ được kiểm tra
+        ValidateIssuerSigningKey = true,
+
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("my21charSuperSecretKeyForMy21charSuperSecretKey")),
+        //Điều khiển việc xác thực giá trị audience trong token. Nếu giá trị này là false, giá trị audience sẽ không được kiểm tra
+        ValidateAudience = false,
+        //Điều khiển việc xác thực giá trị issuer trong token. Nếu giá trị này là false, giá trị issuer sẽ không được kiểm tra
+        ValidateIssuer = false,
+
+        //kết hợp với Expires trong UserController để ra time chính xác 
+        ClockSkew = TimeSpan.Zero,
+    };
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -27,7 +59,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
