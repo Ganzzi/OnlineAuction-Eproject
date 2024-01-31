de05c18f68d9e02bbffc23f9587391466e72eac7
Ganzzi <boinguyen9701@gmail.com>
2024-01-31 18:52:16 +0700

    update signup page

--- ClientApp/app/(authentication)/auth/signup/page.tsx
+++ ClientApp/app/(authentication)/auth/signup/page.tsx
@@ -77,8 +77,6 @@ const SignUp: React.FC = () => {
   }
   return (
     <>
-      <Breadcrumb pageName="Sign Up" />
-
       <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
         <div className="flex flex-wrap items-center">
           <LeftSide />
